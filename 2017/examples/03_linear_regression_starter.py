e2bc1feb287addf6d26545288cc1eea2e7747487
pss <deserthike@gmail.com>
2018-01-30 16:33:15 -0800

    Ignoring 2017 material

--- 2017/examples/03_linear_regression_starter.py
+++ 2017/examples/03_linear_regression_starter.py
@@ -27,40 +27,50 @@ n_samples = sheet.nrows - 1
 # Step 2: create placeholders for input X (number of fire) and label Y (number of theft)
 # Both have the type float32
 
+X = tf.placeholder(dtype = tf.float32, name = 'X')
+Y = tf.placeholder(dtype = tf.float32, name = 'Y')
+
+
 
 # Step 3: create weight and bias, initialized to 0
 # name your variables w and b
 
+w = tf.Variable(0.0, name = 'w')
+b = tf.Variable(0.0, name = 'b')
+
 
 # Step 4: predict Y (number of theft) from the number of fire
 # name your variable Y_predicted
+Y_predicted = tf.add(tf.multiply(X, w), b)
 
 
 # Step 5: use the square error as the loss function
 # name your variable loss
-
+loss = tf.square(tf.subtract(Y, Y_predicted))
 
 # Step 6: using gradient descent with learning rate of 0.01 to minimize loss
- 
+optimizer = tf.train.GradientDescentOptimizer(0.001).minimize(loss)
+
 # Phase 2: Train our model
 with tf.Session() as sess:
-	# Step 7: initialize the necessary variables, in this case, w and b
-	# TO - DO	
+#Step 7: initialize the necessary variables, in this case, w and b
+# TO - DO
+	sess.run(tf.global_variables_initializer())
 
-
-	# Step 8: train the model
-	for i in range(50): # run 100 epochs
+# Step 8: train the model
+	for i in range(100): # run 100 epochs
 		total_loss = 0
 		for x, y in data:
-			# Session runs optimizer to minimize loss and fetch the value of loss. Name the received value as l
-			# TO DO: write sess.run()
-
+# Session runs optimizer to minimize loss and fetch the value of loss. Name the received value as l
+# TO DO: write sess.run()
+			_, l = sess.run([optimizer, loss], feed_dict={X: x, Y:y})
 			total_loss += l
 		print("Epoch {0}: {1}".format(i, total_loss/n_samples))
-	
-# plot the results
-# X, Y = data.T[0], data.T[1]
-# plt.plot(X, Y, 'bo', label='Real data')
-# plt.plot(X, X * w + b, 'r', label='Predicted data')
-# plt.legend()
-# plt.show()
+	w_final, b_final = sess.run([w, b])
+#plot the results
+X, Y = data.T[0], data.T[1]
+print(w_final)
+plt.plot(X, Y, 'bo', label='Real data')
+plt.plot(X, X * w_final + b_final, 'r', label='Predicted data')
+plt.legend()
+plt.show()
